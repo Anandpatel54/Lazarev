534591ed910f0b84204a4caac455a0338fec063b
Anand patel <panand054@gmail.com>
2024-05-02 12:48:47 +0530

    page 6 part

--- script.js
+++ script.js
@@ -83,6 +83,18 @@ function makeVideopart() {
   });
 }
 
+var sections = document.querySelectorAll(".sec-right");
+sections.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    elem.childNodes[3].style.opacity = 1;
+    elem.childNodes[3].play();
+  });
+  elem.addEventListener("mouseleave", function () {
+    elem.childNodes[3].style.opacity = 0;
+    elem.childNodes[3].load();
+  });
+});
+
 makeAnimationNav();
 page1Animation();
 makeVideopart();
